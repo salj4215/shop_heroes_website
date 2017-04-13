ab24fd2a8b7ccd20804c792fb04b8d88456c8314
Donald Davis <ddavis68@hawkmail.hfcc.edu>
2017-04-12 20:12:31 -0400

    sal davis
    added require for connection into index. changed link to orders page

--- index.php
+++ index.php
@@ -5,12 +5,25 @@
  * Date: 4/3/2017
  * Time: 9:11 PM
  */
+require_once (connect_db.php);
+
+$charset = 'utf8';
+$dsn = "mysql:host=".HOST.";dbname=".DB.";charset=$charset";
+$opt =
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+$pdo = new PDO($dsn, USER, PASS, $opt); //create pdo object
+
 require ('header.html');
 
 if(isset($_GET['page']) && $_GET['page'] == 'contact')
     require ('contact.html'); //contact page
 elseif (isset($_GET['page']) && $_GET['page'] == 'home')
     require ('HomeSH.html');
+elseif (isset($_GET['page']) && $_GET['page'] == 'order')
+    require ('Order.php');
 else
     require ('HomeSH.html');
 
