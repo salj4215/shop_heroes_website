de55b7d3b7495edf6765bce5f2b04521e89d26a7
Donald Davis <ddavis68@hawkmail.hfcc.edu>
2017-04-12 20:04:55 -0400

    sal davis
    half way, testing connection with pulling products

--- order.php
+++ order.php
@@ -1,24 +1,3 @@
-<?php
-    /**
-     * Created by Salleh Jahaf
-     * Description: Shop Heroes Order page where customer places items into shopping cart.
-     * Date: 4/6/2017
-     * Time: 9:46 PM
-     */
-
-    require_once ('../connect_db.php'); //waiting for access to file
-
-$charset = 'utf8';
-$dsn = "mysql:host=".HOST.";dbname=".DB.";charset=$charset";
-$opt =
-    [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
-$pdo = new PDO($dsn, USER, PASS, $opt); //create pdo object
-
-?>
-
 <div id="prod_navigation">
     <div class="dropdown"> <!-- drop-down button -->
         <button class="productbtn">Products</button>
@@ -31,3 +10,20 @@ $pdo = new PDO($dsn, USER, PASS, $opt); //create pdo object
     </div>
     <button class="chkoutbtn">Check-Out</button>
 </div>
+
+<?php
+	$qry = 'Select * from `Products`';
+	
+	$stmt = $pdo -> exec( $qry );
+	
+	while($row = $stmt->fetch())
+	{
+		echo $row['ProductID'] . "<br>";
+		echo $row['StoreID'] . "<br>";;
+		echo $row['ProductName'] . "<br>";;
+		echo $row['ProductUPC'] . "<br>";;
+		echo $row['UnitPrice'] . "<br>";;
+		echo $row['ProductCategory'] . "<br>";;
+		echo $row['Description'] . "<br>";;
+		echo $row['Quantity'] . "<br>";;		
+	}	
