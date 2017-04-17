ddc75780b761dea3b7cfd44b8341c0ca5a8f3c3d
RB <RB>
2017-04-17 10:44:03 -0400

    by rebienenstein Updated retrieveItems for StoreID

--- api/retrieveItems.php
+++ api/retrieveItems.php
@@ -5,7 +5,7 @@ require_once ('../connect_db.php');
 
 $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 
-$sql = "SELECT ProductName, UnitPrice, ProductUPC, ProductCategory from PRODUCTS";
+$sql = "SELECT StoreID, ProductName, UnitPrice, ProductUPC, ProductCategory from PRODUCTS WHERE StoreID != 0";
 
 $itemlist = mysqli_query($con,$sql);
 
@@ -13,6 +13,7 @@ $item_arr = array();
 
 while ($row = mysqli_fetch_array($itemlist)) {
     array_push($item_arr,array(
+        "storeID"=>$row['StoreID'],
         "itemName"=>$row['ProductName'],
         "itemUPC"=>$row['ProductUPC'],
         "itemPrice"=>$row['UnitPrice'],
