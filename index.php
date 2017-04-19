198777bb4ec365b383bb7d539e4979ae5107c977
Donald Davis <ddavis68@hawkmail.hfcc.edu>
2017-04-19 19:33:41 -0400

    Davis
    Removed header.php, header.html.
    Made header.phtml

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ if( isset( $_POST['action'] ) && $_POST['action'] == 'login') {
         }
     }
 }
-require ('header.html');
+require ('header.phtml');
 //test vairables
 if( isset( $_POST['action'] ) && $_POST['action'] == 'login') {
 var_dump($_POST['myusername']);
