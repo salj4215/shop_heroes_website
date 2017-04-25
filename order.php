7ee35645dcb509ed52103456feeba0cc458b608d
Donald Davis <ddavis68@hawkmail.hfcc.edu>
2017-04-25 18:32:15 -0400

    Davis::orders display store
    dispaly store for items

--- order.php
+++ order.php
@@ -151,7 +151,7 @@ function AddToCart($pid)
 	    $productID = $row['ProductID'];
 		echo "<td>ProductName: " . $row['ProductName'] . "<br><br>ProductCategory: " . $row['ProductCategory'] . "<br><br>ProductUPC: " . $row['ProductUPC'] . "</td>";
 		echo "<td>Description: " . $row['Description'] . "</td>";
-        echo "<td>UnitPrice: $" . $row['UnitPrice'] . "<br><br>Quantity: " . $row['Quantity'] . "</td>";
+        echo "<td>UnitPrice: $" . $row['UnitPrice'] . "<br><br>Quantity: " . $row['Quantity'] . "<br><br>Store= "; if($row['StoreID'] == 1){echo "Kroger";}; if($row['StoreID'] == 2){echo 'Meijer';}; echo "</td>";
 //        echo "<td><a href='index.php?page=order&pid=$productID'>ADD TO CART</a></td></tr>";
         echo "<td><form name='addToCart' action='index.php?page=order' method='POST'><input type='hidden' name='productID' value='$productID'>";
         echo "<input type='submit' name='cartBtn' value='ADD TO CART'></td></tr>";
