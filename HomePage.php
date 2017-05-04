efc2a6002043da8db3915139cf451d7941aa39e1
Donald Davis <ddavis68@hawkmail.hfcc.edu>
2017-05-04 19:44:08 -0400

    Davis:: view employee

--- HomePage.php
+++ HomePage.php
@@ -3,7 +3,9 @@ include('index.php');
 $qry = "SELECT * FROM USERS JOIN EMPLOYEES ON USERS.UserID = EMPLOYEES.UserID";
 $stmt = $pdo->query($qry);
 while ($row = $stmt->fetch()) {
-echo $row;
+?>
+<pre><?php print_r($row); ?></pre>
+<?php
 }
 
 ?>
