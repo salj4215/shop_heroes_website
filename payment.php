3dd120f9c1724cd22526ff65c0ac0fffc1c2f939
saljay <sjahaf@hawkmail.hfcc.edu>
2017-05-03 20:11:37 -0400

    davis: sal spacing in td

--- payment.php
+++ payment.php
@@ -64,7 +64,7 @@ $grandTotal = $subTotal + $shipping;
 ?>
 <div id="payment">
 
-    <table class="payment" align = "center" cellpadding="0" cellspacing="0">
+    <table class="no-spacing" cellspacing="0">
         <form name="confirmCheckout" action="processOrder.php"  method="POST">
             <td>
                 <fieldset><legend align="center"> Shipping Address :</legend>
