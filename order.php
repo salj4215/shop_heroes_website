4f55e3a3ecd7da47643d079f2aebcee15da1715b
sjahaf <sjahaf@hawkmail.hfcc.edu>
2017-05-05 01:17:39 -0400

    adding javascript to alert user item was added

--- order.php
+++ order.php
@@ -1,3 +1,6 @@
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
 <link rel="stylesheet" type="text/css" href="styleOrder.css">
 <div id="shopPage">
 <?php
